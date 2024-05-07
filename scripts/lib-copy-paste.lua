85e6e2918580afd47dfbc993490ef91df8e3e615
Maxim Biro <nurupo.contributions@gmail.com>
2024-05-06 21:55:24 -0400

    Rewrite the copy-paste library script
    - Use mpv's subprocess functionality over Lua's io.popen() to avoid
      cmd.exe pop-ups
    - Properly escape the copied strings to avoid command injection
    - Use mpv's platform detection functionality

--- scripts/lib-copy-paste.lua
+++ scripts/lib-copy-paste.lua
@@ -22,17 +22,70 @@
 
 -- Module for copying and pasting.
 --
--- Linux and Windows only. On Linux, requires xclip to be installed. On Windows, requires Windows 10 (no idea if it works on earlier Windowses).
+-- Linux and Windows only.
+-- On Linux, requires xclip to be installed.
+-- On Windows, requires powershell and tested to work on Windows 10.
 
 local cp = {}
 
-local powershell_utf8 = 'powershell -window hidden -command "$OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding; '
+local powershell_utf8 = '$OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding; '
 
-local copy_cmd = package.config:sub(1,1) == '\\' and         powershell_utf8 .. 'Get-Clipboard"' or -- Windows
-                 os.execute('xclip -h > /dev/null 2>&1') and 'xclip -selection clipboard -o'        -- Linux
-local paste_fn = function (str) return  package.config:sub(1,1) == '\\' and         os.execute(powershell_utf8 .. 'Write-Host -NoNewLine ""' .. str .. '""" | clip.exe') or -- Windows
-                                        os.execute('xclip -h > /dev/null 2>&1') and os.execute("echo -n '" .. str:gsub("'", "'\\''") .. "' | xclip -selection clipboard")   -- Linux
-                 end
+-- gets a string as an argument. it's also provided as the stdin.
+-- if using the argument, make sure to escape it well, to prevent command
+-- injections. prefer using the stdin to avoid the headache of argument
+-- escaping.
+-- Note that stdin doesn't work on Windows yet: https://github.com/mpv-player/mpv/issues/8503
+local set_clipboard_cmd_fns = {
+    windows = function(s) return {'powershell', '-command', powershell_utf8 .. "Set-Clipboard -Value '" .. s:gsub("'", "''") .. "'"} end,
+    linux   = function(s) return {'xclip', '-selection', 'clipboard', '-i'} end,
+}
+
+local get_clipboard_cmd_fns = {
+    windows = function() return {'powershell', '-command', powershell_utf8 .. 'Get-Clipboard'} end,
+    linux   = function() return {'xclip', '-selection', 'clipboard', '-o'} end,
+}
+
+local function get_platform_value(platform_table)
+    local platform = mp.get_property_native('platform')
+    if not platform_table[platform] then
+        mp.osd_message('Error: Unsupported platform "' .. platform .. '"', 10)
+        mp.msg.error('Unsupported platform "' .. platform .. '"')
+        return nil
+    end
+    return platform_table[platform]
+end
+
+local function run_platform_cmd(cmd_fn_platform_table, cmd_fn_args, cmd_stdin, operation)
+    local cmd_fn = get_platform_value(cmd_fn_platform_table)
+    if not cmd_fn then
+        return false, nil
+    end
+    local cmd = cmd_fn(unpack(cmd_fn_args))
+    -- use mpv's subproccess command over Lua's io.popen(), to avoid the
+    -- cmd.exe window poping up on Windows
+    local result = mp.command_native({
+        name = "subprocess",
+        playback_only = false,
+        args = cmd,
+        capture_stdout = true,
+        capture_stderr = true,
+        stdin_data = cmd_stdin,
+    })
+    if not result or result.killed_by_us or result.status ~= 0 then
+        mp.osd_message('Error: Failed to ' .. operation .. '!', 10)
+        mp.msg.error('Failed to ' .. operation .. '!')
+        if result then
+            if result.stderr then
+                mp.msg.error(result.stderr)
+            end
+            if result.error_string == 'init' then
+                mp.msg.error('Couldn\'t run "' .. cmd[1] .. '", is the program installed?')
+            end
+        end
+        return false, nil
+    end
+    return true, result.stdout
+end
 
 function cp.copy(str, verbose)
     if not str then
@@ -40,9 +93,8 @@ function cp.copy(str, verbose)
         mp.msg.error('Failed to copy - nothing to copy!')
         return false
     end
-    if not paste_fn(str) then
-        mp.osd_message('Error: Failed to copy! Do you have xclip installed?', 10)
-        mp.msg.error('Failed to copy! Do you have xclip installed?')
+    local success, _ = run_platform_cmd(set_clipboard_cmd_fns, {str}, str, 'copy')
+    if not success then
         return false
     end
     if verbose then
@@ -53,13 +105,15 @@ function cp.copy(str, verbose)
 end
 
 function cp.paste(verbose)
-    local p = io.popen(copy_cmd, 'r')
-    -- remove "file://" if present
-    local str = p:read()
-    if not p:close() then
-        mp.osd_message('Error: Failed to paste! Do you have xclip installed?', 10)
-        mp.msg.error('Failed to paste! Do you have xclip installed?')
+    local success, str = run_platform_cmd(get_clipboard_cmd_fns, {}, nil, 'paste')
+    if not success then
         return nil
+    end
+    if mp.get_property_native('platform') == 'windows' then
+        -- remove the trailing \r\n
+        if str:len() >= 2 and str:sub(-2, -1) == '\r\n' then
+            str = str:sub(1, -3)
+        end
     end
     if verbose then
         mp.osd_message('Pasted: ' .. str, 3)
