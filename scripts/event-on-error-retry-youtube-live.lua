140508e2606d7bde37f75354c391a6d0538e1c66
Maxim Biro <nurupo.contributions@gmail.com>
2024-05-18 23:06:51 -0400

    Require mp.msg in the youtube live retry script

--- scripts/event-on-error-retry-youtube-live.lua
+++ scripts/event-on-error-retry-youtube-live.lua
@@ -25,7 +25,8 @@
 -- Requires curl to be installed (surprisingly, at some point Windows 10 started including curl)
 
 package.path = debug.getinfo(1).source:match("@?(.*/)") .. '?.lua;' .. package.path
-local wv = require('lib-web-video')
+local wv  = require 'lib-web-video'
+local msg = require 'mp.msg'
 
 local youtube_id
 local path
@@ -40,7 +41,7 @@ local function record_youtube_id()
         path = mp.get_property('path')
         original_idle = mp.get_property('idle')
         if not overrode_idle then
-            mp.msg.verbose("Temporarily setting idle=yes as it's required for the script to work.")
+            msg.verbose("Temporarily setting idle=yes as it's required for the script to work.")
             mp.set_property('idle', 'yes')
             overrode_idle = true
         end
@@ -49,7 +50,7 @@ end
 
 local function restore_idle()
     if overrode_idle then
-        mp.msg.verbose("Restoring idle=" .. original_idle)
+        msg.verbose("Restoring idle=" .. original_idle)
         mp.set_property('idle', original_idle)
         overrode_idle = false
     end
@@ -65,11 +66,11 @@ local function http_code(url)
         capture_stderr = true,
     })
     if not result or result.status ~= 0 then
-        mp.msg.warn("Failed to get the HTTP code of " .. url)
+        msg.warn("Failed to get the HTTP code of " .. url)
         if result then
-            mp.msg.warn('status: ' .. result.status)
-            mp.msg.warn('stdout: ' .. result.stdout)
-            mp.msg.warn('stderr: ' .. result.stderr)
+            msg.warn('status: ' .. result.status)
+            msg.warn('stdout: ' .. result.stdout)
+            msg.warn('stderr: ' .. result.stderr)
         end
         return nil
     end
@@ -85,7 +86,7 @@ local function retry_youtube_live(event)
         return
     end
     if youtube_id and youtube_has_live_thumbnail(youtube_id) then
-        mp.msg.info("This appears to be an upcoming YouTube live stream. Will keep retying to open it until it goes live.")
+        msg.info("This appears to be an upcoming YouTube live stream. Will keep retying to open it until it goes live.")
         mp.add_timeout(8.5, function()
             mp.commandv('loadfile', path)
         end)
